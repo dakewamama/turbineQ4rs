f7758c2c5487b21958a0443bf7c126b594cbfab0
dakewamama <davidasiadiachi@gmail.com>
2025-10-01 21:31:01 +0000

    keygen done

---
+++ airdrop2/src/lib.rs
@@ -0,0 +1,17 @@
+#[cfg(test)]
+
+mod tests {
+    use solana_sdk::signature::{Keypair, Signer};
+    #[test]
+fn keygen() {
+    // Create a new keypair
+    let kp = Keypair::new();
+    println!("You've generated a new Solana wallet: {}\n", kp.pubkey());
+    println!("To save your wallet, copy and paste the following into a JSON file:");
+    println!("{:?}", kp.to_bytes());
+    }
+    #[test]
+    fn claim_airdrop() {}
+    #[test]
+    fn transfer_sol() {}
+}
